55b1bfbc12409f9ae4579c6ec055c276cfb05f20
dheemanthpapolu <dheemanth9805@gmail.com>
2025-06-30 15:02:28 +0530

    Added complexity_info.py inside core/

---
+++ core/complexity_info.py
@@ -0,0 +1,93 @@
+complexity_data = {
+    "Bubble Sort": {
+        "best": "O(n)",
+        "avg": "O(n^2)",
+        "worst": "O(n^2)",
+        "space": "O(1)",
+        "stable": "Yes ✅"
+    },
+    "Selection Sort": {
+        "best": "O(n^2)",
+        "avg": "O(n^2)",
+        "worst": "O(n^2)",
+        "space": "O(1)",
+        "stable": "No ❌"
+    },
+    "Insertion Sort": {
+        "best": "O(n)",
+        "avg": "O(n^2)",
+        "worst": "O(n^2)",
+        "space": "O(1)",
+        "stable": "Yes ✅"
+    },
+    "Merge Sort": {
+        "best": "O(n log n)",
+        "avg": "O(n log n)",
+        "worst": "O(n log n)",
+        "space": "O(n)",
+        "stable": "Yes ✅"
+    },
+    "Quick Sort": {
+        "best": "O(n log n)",
+        "avg": "O(n log n)",
+        "worst": "O(n^2)",
+        "space": "O(log n)",
+        "stable": "No ❌"
+    },
+    "Heap Sort": {
+        "best": "O(n log n)",
+        "avg": "O(n log n)",
+        "worst": "O(n log n)",
+        "space": "O(1)",
+        "stable": "No ❌"
+    },
+    "Counting Sort": {
+        "best": "O(n + k)",
+        "avg": "O(n + k)",
+        "worst": "O(n + k)",
+        "space": "O(k)",
+        "stable": "Yes ✅"
+    },
+    "Radix Sort": {
+        "best": "O(nk)",
+        "avg": "O(nk)",
+        "worst": "O(nk)",
+        "space": "O(n + k)",
+        "stable": "Yes ✅"
+    },
+    "Shell Sort": {
+        "best": "O(n log n)",
+        "avg": "O(n log^2 n)",
+        "worst": "O(n^2)",
+        "space": "O(1)",
+        "stable": "No ❌"
+    },
+    "Comb Sort": {
+        "best": "O(n log n)",
+        "avg": "O(n^2)",
+        "worst": "O(n^2)",
+        "space": "O(1)",
+        "stable": "No ❌"
+    },
+    "Tree Sort": {
+        "best": "O(n log n)",
+        "avg": "O(n log n)",
+        "worst": "O(n^2)",
+        "space": "O(n)",
+        "stable": "No ❌"
+    },
+    "Bucket Sort": {
+        "best": "O(n + k)",
+        "avg": "O(n + k)",
+        "worst": "O(n^2)",
+        "space": "O(n + k)",
+        "stable": "Yes ✅"
+    },
+    "Tim Sort": {
+        "best": "O(n)",
+        "avg": "O(n log n)",
+        "worst": "O(n log n)",
+        "space": "O(n)",
+        "stable": "Yes ✅"
+    }
+}

5fcdfae7e9e7b734754f7cb9033bfdbc84f5f9c2
dheemanthpapolu <dheemanth9805@gmail.com>
2025-06-30 15:07:31 +0530

    Create heap_sort.py

---
+++ algorithms/heap_sort.py
@@ -0,0 +1,23 @@
+def heap_sort(arr):
+    def heapify(arr, n, i):
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < n and arr[l] > arr[largest]:
+            largest = l
+        if r < n and arr[r] > arr[largest]:
+            largest = r
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            yield arr, [i, largest]
+            yield from heapify(arr, n, largest)
+
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        yield from heapify(arr, n, i)
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        yield arr, [0, i]
+        yield from heapify(arr, i, 0)
+
+    yield arr, []

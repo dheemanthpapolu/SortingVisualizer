6093831f39d29d9826bdb6ef77ff0aa3c239916d
dheemanthpapolu <dheemanth9805@gmail.com>
2025-06-30 15:04:14 +0530

    Added sorter.py inside core/

---
+++ core/sorter.py
@@ -0,0 +1,29 @@
+from algorithms.bubble_sort import bubble_sort
+from algorithms.insertion_sort import insertion_sort
+from algorithms.selection_sort import selection_sort
+from algorithms.merge_sort import merge_sort
+from algorithms.quick_sort import quick_sort
+from algorithms.heap_sort import heap_sort
+from algorithms.counting_sort import counting_sort
+from algorithms.radix_sort import radix_sort
+from algorithms.shell_sort import shell_sort
+from algorithms.comb_sort import comb_sort
+from algorithms.tree_sort import tree_sort
+from algorithms.bucket_sort import bucket_sort
+from algorithms.tim_sort import tim_sort
+
+algorithm_map = {
+    "Bubble Sort": bubble_sort,
+    "Insertion Sort": insertion_sort,
+    "Selection Sort": selection_sort,
+    "Merge Sort": merge_sort,
+    "Quick Sort": quick_sort,
+    "Heap Sort": heap_sort,
+    "Counting Sort": counting_sort,
+    "Radix Sort": radix_sort,
+    "Shell Sort": shell_sort,
+    "Comb Sort": comb_sort,
+    "Tree Sort": tree_sort,
+    "Bucket Sort": bucket_sort,
+    "Tim Sort": tim_sort,
+}
